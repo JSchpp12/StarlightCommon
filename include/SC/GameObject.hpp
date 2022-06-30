1ffb28be7f4bba3a56ff1a175a584082da608dec
Jacob Schaupp <jacob.schaupp@gmail.com>
2022-06-30 16:17:54 -0400

    Create and add material to gameobjects

--- include/SC/GameObject.hpp
+++ include/SC/GameObject.hpp
@@ -25,10 +25,10 @@ namespace star{
         public:
             GameObject(std::unique_ptr<std::vector<Vertex>> vertexList, std::unique_ptr<std::vector<uint32_t>> indiciesList,
                 glm::vec3 position, glm::vec3 scale, 
-                common::Material& material, Handle& vertShaderHandle,
+                common::Material* material, Handle& vertShaderHandle,
                 Handle& fragShaderHandle, Handle& textureHandle) :
                 Entity(), 
-                material(std::make_unique<common::Material>(material)),
+                material(material),
                 vertexList(std::move(vertexList)),
                 indiciesList(std::move(indiciesList)),
                 vertShader(std::make_unique<Handle>(vertShaderHandle)),
@@ -69,7 +69,7 @@ namespace star{
 
             std::vector<uint32_t>* getIndicies() { return this->indiciesList.get(); }
 
-            Material* getMaterial() { return this->material.get(); }
+            Material* getMaterial() { return this->material; }
 
         protected: 
 
@@ -80,7 +80,7 @@ namespace star{
             std::unique_ptr<Handle> vertShader, fragShader, texture; 
             std::unique_ptr<std::vector<common::Vertex>> vertexList;
             std::unique_ptr<std::vector<uint32_t>> indiciesList;
-            std::unique_ptr<common::Material> material; 
+            common::Material* material; 
         };
     }
 }
--- include/SC/Material.hpp
+++ include/SC/Material.hpp
@@ -7,11 +7,12 @@ namespace common {
 	struct Material {
 		Material() = default; 
 
-		Material(glm::vec4 surfaceColor, glm::vec4 highlightColor) :
-			surfaceColor(surfaceColor), highlightColor(highlightColor) { }
+		Material(glm::vec4 surfaceColor, glm::vec4 highlightColor, int shinyCoefficient) :
+			surfaceColor(surfaceColor), highlightColor(highlightColor), shinyCoefficient(shinyCoefficient) { }
 
 		glm::vec4 surfaceColor{0.5f, 0.5f, 0.5f, 1.0f};
 		glm::vec4 highlightColor{ 0.5f, 0.5f, 0.5f, 1.0f };
+		int shinyCoefficient = 1; 
 	};
 }
 }
