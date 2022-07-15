1b7a9faf5ef8f5be270141110b3952d2dfae9fd6
Jacob Schaupp <js400421@ohio.edu>
2022-07-15 16:25:27 -0400

    Add light properties

--- include/SC/Light.hpp
+++ include/SC/Light.hpp
@@ -10,21 +10,37 @@
 namespace star::common{
 	class Light : public Entity{
 	public:
-		Light(common::Type::Light type) : Entity(), type(type) { }
+		Light(Type::Light type, glm::vec3 position) : Entity(position) {
+			this->type = type; 
+		}
 
-		Light(Type::Light type, glm::vec3 position) : Entity(position), type(type) { }
-
-		Light(Type::Light type, glm::vec3 position, glm::vec4 color) : Entity(position), type(type), color(color) { }
-
-		Light(Type::Light type, glm::vec3 position, glm::vec4 color, 
-			glm::vec3 scale) : Entity(position) {}
-
-		Light(Type::Light type, glm::vec3 position, glm::vec4 color,
-			glm::vec3 scale, common::Handle linkedObjectHandle,
-			common::GameObject* linkedObject) : 
-			Entity(position, scale), type(type), 
-			color(color), linkedObjectHandle(std::make_unique<common::Handle>(linkedObjectHandle)), 
-			linkedObject(linkedObject) { }
+		//create light with no linked object
+		Light(Type::Light type, glm::vec3 position, const glm::vec4* ambient = nullptr, 
+			const glm::vec4* diffuse = nullptr, const glm::vec4* specular = nullptr) 
+			: Entity(position){ 
+			this->type = type; 
+			if (ambient != nullptr)
+				this->ambient = *ambient; 
+			if (diffuse != nullptr)
+				this->diffuse = *diffuse; 
+			if (specular != nullptr)
+				this->specular = *specular; 
+		}
+		//create light with linked object
+		Light(Type::Light type, glm::vec3 position, glm::vec3 scale,
+			Handle linkedObjectHandle, GameObject& linkedObject, 
+			const glm::vec4* ambient = nullptr, const glm::vec4* diffuse = nullptr,
+			const glm::vec4* specular = nullptr) :
+			Entity(position, scale), linkedObjectHandle(std::make_unique<Handle>(linkedObjectHandle)) {
+			this->type = type;
+			this->linkedObject = &linkedObject;
+			if (ambient != nullptr)
+				this->ambient = *ambient; 
+			if (diffuse != nullptr)
+				this->diffuse = *diffuse; 
+			if (specular != nullptr)
+				this->specular = *specular; 
+		}
 		
 		//check if the light has a linked render object
 		bool hasObject(){
@@ -40,19 +56,23 @@ namespace star::common{
 			this->Entity::moveRelative(movement);
 		}
 
-		void setLinkedObjectHandle(common::Handle handle) { this->linkedObjectHandle = std::make_unique<common::Handle>(handle); }
-		void setLinkedObject(common::GameObject& object) { this->linkedObject = &object; }
-		common::Handle getLinkedObjectHandle() { return *this->linkedObjectHandle; }
+		void setLinkedObjectHandle(Handle handle) { this->linkedObjectHandle = std::make_unique<Handle>(handle); }
+		void setLinkedObject(GameObject& object) { this->linkedObject = &object; }
+		Handle getLinkedObjectHandle() { return *this->linkedObjectHandle; }
 		Type::Light getType() { return this->type; }
-		glm::vec4 getColor() { return this->color; }
+		glm::vec4 getAmbient() { return this->ambient; }
+		glm::vec4 getDiffuse() { return this->diffuse; }
+		glm::vec4 getSpecular() { return this->specular; }
 
 	private:
 		//handle to the object that will be rendered along with the light (positional object such as billboard)
-		std::unique_ptr<common::Handle> linkedObjectHandle;
-		common::GameObject* linkedObject = nullptr; 
+		std::unique_ptr<Handle> linkedObjectHandle;
+		GameObject* linkedObject = nullptr; 
 
 		Type::Light type;
-		glm::vec4 color = glm::vec4{1.0f, 1.0f, 1.0f, 1.0f};
+		glm::vec4 ambient = glm::vec4{0.5f, 0.5f, 0.5f, 1.0f};
+		glm::vec4 diffuse = glm::vec4{0.5f, 0.5f, 0.5f, 1.0f};
+		glm::vec4 specular = glm::vec4{0.5f, 0.5f, 0.5f, 1.0f};
 
 	};
 }
