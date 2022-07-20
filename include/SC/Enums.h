737e2a4068f6d6c292e4e5016c7d6b7e73f99cc0
Jacob Schaupp <jacob.schaupp@gmail.com>
2022-07-20 13:07:43 -0400

    Add Render_Setting Enum

--- include/SC/Enums.h
+++ include/SC/Enums.h
@@ -1,56 +1,61 @@
 #pragma once
 
-namespace star{
-    namespace common{
-        enum Shader_File_Type{
-            spirv, 
-            glsl
-        }; 
+namespace star::common{
+    enum Shader_File_Type{
+        spirv, 
+        glsl
+    }; 
 
 
-        enum Shader_Stage {
-            vertex,
-            fragment,
-            compute
-        };
+    enum Shader_Stage {
+        vertex,
+        fragment,
+        compute
+    };
 
-        enum Config_Settings{
-            mediadirectory
-        }; 
+    enum Config_Settings{
+        mediadirectory
+    }; 
 
-        enum Handle_Type{
-            null,
-            defaultHandle,
-            shader, 
-            object, 
-            texture, 
-            material, 
+    enum Handle_Type{
+        null,
+        defaultHandle,
+        shader, 
+        object, 
+        texture, 
+        material, 
+        light
+    }; 
+
+    enum Buttons {
+        STAR_W, 
+        STAR_A, 
+        STAR_S, 
+        STAR_D,
+        STAR_CLICK_LEFT, 
+        STAR_CLICK_RIGHT, 
+        STAR_UP, 
+        STAR_DOWN, 
+        STAR_LEFT,
+        STAR_RIGHT
+    };
+
+    namespace Type {
+        enum Entity {
+            model, 
             light
-        }; 
-
-        enum Buttons {
-            STAR_W, 
-            STAR_A, 
-            STAR_S, 
-            STAR_D,
-            STAR_CLICK_LEFT, 
-            STAR_CLICK_RIGHT, 
-            STAR_UP, 
-            STAR_DOWN, 
-            STAR_LEFT,
-            STAR_RIGHT
         };
 
-        namespace Type {
-            enum Entity {
-                model, 
-                light
-            };
-
-            enum Light {
-                point,
-                directional
-            };
-        }
+        enum Light {
+            point,
+            directional
+        };
+    }
+        
+    namespace Render_Settings {
+        enum Material {
+            none, 
+            ambient
+        };
     }
 }
