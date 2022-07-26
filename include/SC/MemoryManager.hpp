cf113940e438ff62aba8b54504f903d417c20b15
Jacob Schaupp <jacob.schaupp@gmail.com>
2022-07-26 19:17:50 -0400

    Cleanup + Comments

--- include/SC/MemoryManager.hpp
+++ include/SC/MemoryManager.hpp
@@ -16,7 +16,12 @@ namespace star::common{
 		/// <param name="defaultResource"></param>
 		MemoryManager(std::unique_ptr<T> defaultResource){ }
 		virtual ~MemoryManager() { };
-
+		/// <summary>
+		/// Add a new resource to the manager. This resource needs to be provided as a unique_ptr in order to prevent implicit copies and give
+		/// ownership of the object to the manager. This ownership will be passed onto the resource container.
+		/// </summary>
+		/// <param name="newResource"></param>
+		/// <returns></returns>
 		virtual Handle addResource(std::unique_ptr<T> newResource) {
 			Handle newHandle = this->createAppropriateHandle();
 			newHandle.containerIndex = this->container.size();
@@ -24,29 +29,32 @@ namespace star::common{
 			this->container.add(std::move(newResource));
 			return newHandle;
 		}
-		T& resource(const common::Handle& handle) {
-			return this->container.get(handle);
+		/// <summary>
+		/// Returns a reference to the requested resource that is in the manager. This is best to use if the resource needs to be directly changed before rendering operations.
+		/// </summary>
+		/// <param name="handle">Handle to the resource being requested</param>
+		/// <returns>Reference to the object</returns>
+		virtual T& resource(const common::Handle& handle) {
+			if (handle.type == common::Handle_Type::defaultHandle && defaultResource != nullptr) {
+				return *defaultResource;
+			}
+			else {
+				return this->container.get(handle);
+			}
 		}
-		virtual T* getDefault() {
-			if (this->defaultResource != nullptr)
-				return this->defaultResource;
-			throw std::runtime_error("No default resource was set");
-		}
-
-		common::Handle getDefaultHandle() { return *this->defaultHandle; }
 
 	protected:
+		T* defaultResource = nullptr;
 		//Inherited classes should be permitted to inherit from this class without default material reqs. It is expected these classes will handle their own defaults.
 		MemoryManager() = default;
-
-		void init(std::unique_ptr<T> defaultResource) {
+		/// <summary>
+		/// Initalize the MemoryManager with a default resource. 
+		/// </summary>
+		/// <param name="defaultResource"></param>
+		virtual void init(std::unique_ptr<T> defaultResource) {
 			Handle defaultHandle = this->addResource(std::move(defaultResource));
-			this->defaultHandle = std::make_unique<Handle>(defaultHandle);
 			this->defaultResource = &this->resource(defaultHandle);
 		}
-
-		T* defaultResource = nullptr;
-		std::unique_ptr<Handle> defaultHandle;
 
 		/// <summary>
 		/// Create appropriate handle for this manager type 
@@ -57,6 +65,7 @@ namespace star::common{
 		size_t size() { return this->container.size(); }
 
 	private: 
+		//Container for holding the in memory objects that the manager owns.
 		ResourceContainer<T> container;
 
 
