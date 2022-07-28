419230e05f81b768952d231ef2c86e4119dcd09c
Jacob Schaupp <jacob.schaupp@gmail.com>
2022-07-28 13:45:43 -0400

    Addition of new class

---
+++ include/SC/Scene.hpp
@@ -0,0 +1,22 @@
+/*
+* This class contains the interface for the basic scene object that will be provided to the 
+* core rendering system.
+*/
+#pragma once 
+
+
+namespace star::common {
+	class Scene {
+	public:
+		Scene() { }
+		virtual ~Scene(){ }
+
+		
+	protected:
+
+
+	private:
+
+
+	};
+}
